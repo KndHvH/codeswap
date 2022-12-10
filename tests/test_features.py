4b6daf7ffe98e1de69d6645e9073e3745d1a4325
knd <delend89@gmail.com>
2022-12-10 13:32:42 -0300

    85 cov

--- tests/test_features.py
+++ tests/test_features.py
@@ -2,11 +2,8 @@ from service.features.add import add
 from service.features.read import read
 from service.managejson import *
 from service.text.title_verificator import title_verif
-from service.create_db import create_db, FileAlreadyExists
 
 
-import pytest
-import json
 import os
 scriptPath = os.path.dirname(__file__)
 
@@ -18,7 +15,7 @@ class TestClass:
         text = 'text'
         password = 'rtef'
         title = 'test'
-        user = str('1'*300)
+        user = int('1'*300)
 
         expect = 'test'
 
@@ -40,15 +37,21 @@ class TestClass:
         entry = 'text'
         title = 'test3'
         password = 'abcdefghij'
-        user = str('1'*300)
+        user = int('1'*300)
 
-
-        json = add(entry,password,title,user)
+        json = add(entry, password, title, user)
         save_json(json)
 
-        data = read(title,user)
+        data = read(title, user)
 
         assert data == entry
+
+    def test_when_add_receive_less_300_user_return_json(self):
+
+        entry_user = 1233
+        expect = {'title': 'None', 'user': 'None', 'file': 'None'}
+
+        assert expect == add('text', 'pass', 'title', entry_user)
 
     def test_when_title_verif_receive_test4_already_created_return_false(self):
 
@@ -59,9 +62,56 @@ class TestClass:
 
         assert title_verif('test4') == expect
 
-    def test_when_create_file_runs_must_create_empty_json(self):
+    def test_delete_feature(self):
 
-        expect = {'code': []}
+        entry_file_list = [
+            {
+                'title': 'title',
+                'code': 'code',
+                'file': 'file'
+            },
+            {
+                'title': 'mustremove',
+                'code': 'code',
+                'file': 'file'
+            },
+        ]
+
+        entry = {
+            'title': 'mustremove',
+            'code': 'code',
+            'file': 'file'
+        }
+
+        expected = [
+            {
+                'title': 'title',
+                'code': 'code',
+                'file': 'file'
+            }
+        ]
+
+        assert expected == delete(entry_file_list, entry)
+
+    def test_when_save_json_receive_double_files_replace_call_delete_funcion(self):
+
+        entry = {
+            'title': 'title6',
+            'code': 'code',
+            'file': 'oldfilefile'
+        }
+
+        entry2 = {
+            'title': 'title6',
+            'code': 'code',
+            'file': 'newfile'
+        }
+
+        expect = [{
+            'title': 'title6',
+            'code': 'code',
+            'file': 'newfile'
+        }]
 
         relPath = '../database/db.json'
         filepath = os.path.join(scriptPath, relPath)
@@ -70,13 +120,7 @@ class TestClass:
 
         create_db()
 
-        with open(filepath, "r") as file:
-            file = json.load(file)
+        save_json(entry)
+        save_json(entry2, replace=True)
 
-        assert expect == file
-
-    def test_when_create_file_receive_file_already_created_must_return_exception(self):
-
-        with pytest.raises(FileAlreadyExists):
-
-            create_db()
+        assert expect == get_json()['code']
