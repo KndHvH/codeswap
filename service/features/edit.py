ebc6b2ade97f131f824113eab6d0f6422a2747e5
knd <delend89@gmail.com>
2022-12-20 02:53:28 -0300

    fix: windows_colors

--- service/features/edit.py
+++ service/features/edit.py
@@ -1,11 +1,25 @@
-
-from service.text.get_key import get_key
-from service.text.manage_json import *
+from service.features.read import read_file
 from service.features.add import add_file
+from service.text.manage_json import *
+from service.text.get_key import get_key
+import click
+import os
+scriptPath = os.path.dirname(__file__)
 
 
-def edit_file(master, title, user):
+def edit_file(title, user):
 
+    file = read_file(title, user)
+
+    with open('database/temp.txt') as txt:
+        for line in file:
+            txt.write(f'{line}\n')
+
+    master = click.edit(require_save=True, filename='database/temp.txt')
+
+    print(master)
+
+    
     key = get_key(title, user)
 
     json = add_file(master, key, title, user)
