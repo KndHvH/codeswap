9116dc6c6c3a1bbb5652102428e264e51e4bf516
knd <delend89@gmail.com>
2022-12-29 10:36:24 -0300

    feat: auto update

--- service/update.py
+++ service/update.py
@@ -1,11 +1,14 @@
 import subprocess
 
+
 def update():
 
-    subprocess.run(["cd","~/codeswap"])
-    subprocess.run(["git","fetch"])
-    subprocess.run(["git","pull"])
-    subprocess.run(["pip","install","-e","."])
-    
+    # subprocess.run(["cd","~/codeswap"])
+    # subprocess.run(["git","fetch"])
+    # subprocess.run(["git","pull"])
+    # subprocess.run(["pip","install","-e","."])
+    import sys
+    print(sys.path)
+
 
 update()
