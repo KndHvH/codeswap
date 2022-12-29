49f9d66a7564d73ea4738fd2ff1e74a7aa824d4f
Matias Herklotz <101160508+KndHvH@users.noreply.github.com>
2022-12-29 11:18:55 -0300

    auto_update (#16)
    * letter in password
    
    * letter in password
    
    * subprocess test
    
    * refactor
    
    * help
    
    * feat: auto update
    
    * feat: auto update
    
    * feat: auto update
    
    * feat: auto update
    
    * feat: auto update

--- main.py
+++ main.py
@@ -1,60 +1,64 @@
 
 import click
+import os
 
+from service.features.read import read_file
+from service.features.edit import edit_file
+from service.text.manage_json import get_json
 
-from service.menu_features import *
 from service.login import login
+from service.update import update_verif
 from service.version import version_number
 
+update_verif()
 
 @click.group()
 def main():
+    """codeswap:
+
+    \b
+    create or edit your file with the 'file' command.
+    delete your files with the 'delete' command.
+    you can use the '-t' option to input the file title through the terminal.
+
+
+    """
     pass
 
 
-@main.command()
+@main.command(short_help='create or edit a file')
 @click.option('-t', default=None, help='file title')
-def add(t):
+def file(t):
     if not t:
         t = click.prompt(click.style(
             'file title_', fg='blue'), prompt_suffix='')
-    menu_add(t, login())
+    edit_file(t, login())
 
 
-@main.command()
-@click.option('-t', default=None, help='file title')
-def read(t):
-    if not t:
-        t = click.prompt(click.style(
-            'file title_', fg='blue'), prompt_suffix='')
-    menu_read(t, login())
-
-
-@main.command()
-@click.option('-t', default=None, help='file title')
-def edit(t):
-    if not t:
-        t = click.prompt(click.style(
-            'file title_', fg='blue'), prompt_suffix='')
-    menu_edit(t, login())
-
-
-@main.command()
+@main.command(short_help='delete a file')
 @click.option('-t', default=None, help='file title')
 def delete(t):
     if not t:
         t = click.prompt(click.style(
             'file title_', fg='blue'), prompt_suffix='')
-    menu_delete(t, login())
+    click.echo(read_file(t, login()))
+
+    if click.confirm(click.style('confirm delete?', bg='red', fg='white'), prompt_suffix=''):
+        edit_file(t, login(), delete=True)
 
 
-@main.command()
+@main.command(short_help='list all files titles')
 def list():
     click.secho('titles_', fg='blue')
-    for title in menu_list_files():
-        click.echo(title)
+
+    files = get_json()['code']
+
+    for file in files:
+        click.echo(file['title'])
 
 
-@main.command()
+@main.command(short_help='show current version')
 def version():
+    
+    click.echo('install dir:',os.path.dirname(__file__))
     click.echo(f'v{version_number()}')
