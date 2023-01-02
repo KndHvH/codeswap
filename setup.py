be28ad706e39e08f76d856d5ab2b3fb558b7fb09
knd <delend89@gmail.com>
2023-01-02 20:44:43 -0300

    setup.cfg

--- setup.py
+++ setup.py
@@ -4,7 +4,11 @@ from service.version import version_number
 setup(
     name='codeswap',
     version=version_number(),
+    license='MIT',
+    author='Matias Herklotz',
+    author_email='matherklotzz@gmail.com',
     packages=find_packages(),
+    url='https://www.github.com/kndhvh/codeswap',
     include_package_data=True,
     install_requires=[
         'Click',
